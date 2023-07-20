766df1577587596c6c9c593dd1b83b47cfb3268b
sourabh <souraabh@yahoo.com>
2023-07-20 11:31:17 +0530

    home page realignment

--- ecommercefrontend/src/Component/HomeComponent/Home.jsx
+++ ecommercefrontend/src/Component/HomeComponent/Home.jsx
@@ -1,12 +1,18 @@
-
+import "./Home.scss"
 import Banner from "./Banner/Banner"
+import Category from "./Category/Category";
+import Products from "./Products/Products";
 
 export default function Home(){
     return(
-        <div className="Home">
-            <Banner>
-                <div>Home</div>
-            </Banner>
+        <div>
+            <Banner />
+            <div className="main-content">
+                <div className="layout">
+                    <Category /> 
+                    <Products />
+                </div>
+            </div>          
         </div>
     )
 }
